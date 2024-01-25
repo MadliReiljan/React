b952773b0eaebd1ad5670fdc19ddaa1535eeb48a
MadliReiljan <madlireiljan@gmail.com>
2024-01-25 18:36:55 +0200

    1-4 yl

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,24 @@
-function ExpenseItem(){
-	return (<h2>Expense Item</h2>)
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+
+function ExpenseItem(props){
+	const day = props.expenseData.date.toLocaleString
+		('en-US', {day: '2-digit'})
+	const month = props.expenseData.date.
+		toLocaleString('en-US', {month: 'long'})
+	const year = props.expenseData.date.getFullYear()
+
+	return (
+		<div className="expense-item">
+			<ExpenseDate
+			date={props.expenseData.date}
+			/>
+			<div className="expense-item__description">
+				<h2>{props.expenseData.title}</h2>
+				<div className="expense-item__price">{props.expenseData.price}</div>
+			</div>
+		</div>
+	)
 }
 
 export default ExpenseItem

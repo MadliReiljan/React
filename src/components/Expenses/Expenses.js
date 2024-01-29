452ca873607f71d0967af1e31c6fd9e120197fa8
MadliReiljan <madlireiljan@gmail.com>
2024-01-29 18:33:45 +0200

    Väiksed vea parandused ja doc 15 tehtud.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import './Expenses.css'
 import Card from '../UI/Card'
-import ExpenseItem from "./ExpenseItem"
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2024')
@@ -15,20 +15,12 @@ const Expenses = (props) => {
   const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear() == filteredYear
   })
-
-  let expensesContent = <p>No expenses found.</p>
-
-  if(filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-      return <ExpenseItem expenseData={expense} key={expense.id}/>
-    })
-  }
     
     return (
       <Card className="expenses">
-        <ExpensesFilter selected={filteredYear}
-        onChangeFilter={filterChangeHandler}/>
-        {expensesContent}
+        <ExpensesFilter selected={filteredYear} onChangeFilter=
+        {filterChangeHandler}/>
+        <ExpensesList expenses={filteredExpenses}/>
       </Card>
     );
 };

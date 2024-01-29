f91e52c413fe498255817c9202daf6972e6286b8
MadliReiljan <madlireiljan@gmail.com>
2024-01-29 15:21:56 +0200

    Ülesanne 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,16 @@
 import './Expenses.css'
 import Card from '../UI/Card'
 import ExpenseItem from "./ExpenseItem"
+import ExpensesFilter from './ExpensesFilter'
 
 const Expenses = (props) => {
+  const filterChangeHandler = (filteredYear) => {
+    console.log('Year data in Expenses.js ' + filteredYear)
+  }
     
     return (
 		<Card className="expenses">
+      <ExpensesFilter onChangeFilter={filterChangeHandler}/>
 			<ExpenseItem expenseData={props.expenses[0]}/>
       <ExpenseItem expenseData={props.expenses[1]}/>
 		</Card>

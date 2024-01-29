752d61419cf41506d44d291b0325546dfbdd331d
MadliReiljan <madlireiljan@gmail.com>
2024-01-29 17:04:05 +0200

    Vormiandmete kasutamine

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const App = () => {
-
-  const DYMMY_EXPENSES=[
-    {
-        id: 'id1',
-        date: new Date(2023, 1, 10),
-        title: 'New book',
-        price: 30.99
-    },
-    {
-        id: 'id2',
-        date: new Date(2023, 9, 6),
-        title: 'New jeans',
-        price: 99.99
-    },
-    {
-        id: 'id3',
-        date: new Date(2024, 0, 25),
-        title: 'New bag',
-        price: 139.99
-    }
+const DYMMY_EXPENSES=[
+  {
+      id: 'id1',
+      date: new Date(2023, 1, 10),
+      title: 'New book',
+      price: 30.99
+  },
+  {
+      id: 'id2',
+      date: new Date(2023, 9, 6),
+      title: 'New jeans',
+      price: 99.99
+  },
+  {
+      id: 'id3',
+      date: new Date(2024, 0, 25),
+      title: 'New bag',
+      price: 139.99
+  }
 ]
 
+const App = () => {
+  const [expenses, setExpenses] = useState(DYMMY_EXPENSES)
+
 const addExpenseHandler = (expense) => {
-  console.log('In App.js')
-  console.log(expense)
+  setExpenses((previousExpenses) => {
+    return[expense, ...expenses]
+  })
 }
 
   return (
     <div className='App'>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses expenses={DYMMY_EXPENSES} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }

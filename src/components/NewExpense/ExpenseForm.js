3b105eabe034d5ebe3fb3023907caf2cdfd3fb54
MadliReiljan <madlireiljan@gmail.com>
2024-01-30 13:54:30 +0200

    Ül 14

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,18 @@
 import './ExpenseForm.css'
-import { useState, useRef} from 'react'
+import { Fragment, useRef, useState} from 'react'
+import Error from  '../UI/Error'
 
 const ExpenseForm = (props) => {
+    const [error, setError] = useState(null)
+    console.log(error)
+
     const titleInputRef = useRef()
     const amountInputRef = useRef()
     const dateInputRef = useRef()
+
+    const errorHandler = () => {
+        setError(null)
+    }
 
     const submitHandler = (event) => {
         const enteredTitle = titleInputRef.current.value
@@ -12,6 +20,15 @@ const ExpenseForm = (props) => {
         const enteredDate = dateInputRef.current.value
 
         event.preventDefault()
+
+        if(enteredTitle.trim().length == 0 || enteredAmount.trim().length == 0 ||
+        enteredDate.trim().length == 0){
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid title or amount or date (non-empty values)'
+            })
+            return
+        }
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
@@ -25,39 +42,50 @@ const ExpenseForm = (props) => {
     }
 
     return(
-        <form onSubmit={submitHandler}>
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
-                    <label>Title</label>
-                    <input 
-                    type="text" 
-                    id='title'
-                    ref={titleInputRef}/>
+        <Fragment>
+            {error && (
+                <Error
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
+                />
+            )}
+            <div>
+            <form onSubmit={submitHandler}>
+                <div className='new-expense__controls'>
+                    <div className='new-expense__control'>
+                        <label>Title</label>
+                        <input 
+                        type="text" 
+                        id='title'
+                        ref={titleInputRef}/>
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>Amount</label>
+                        <input 
+                        type="number" 
+                        min="0.01" 
+                        step="0.01" 
+                        id='amount'
+                        ref={amountInputRef}/>
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>date</label>
+                        <input 
+                        type="date" 
+                        min="2023-01-01" 
+                        max="2025-01-31" 
+                        id='date'
+                        ref={dateInputRef}/>
+                    </div>
                 </div>
-                <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input 
-                    type="number" 
-                    min="0.01" 
-                    step="0.01" 
-                    id='amount'
-                    ref={amountInputRef}/>
+                <div className='new-expense__actions'>
+                    <button type="submit">Add Expense</button>
+                    <button type='submit'>Cancel</button>
                 </div>
-                <div className='new-expense__control'>
-                    <label>date</label>
-                    <input 
-                    type="date" 
-                    min="2023-01-01" 
-                    max="2025-01-31" 
-                    id='date'
-                    ref={dateInputRef}/>
-                </div>
+            </form>
             </div>
-            <div className='new-expense__actions'>
-                <button type="submit">Add Expense</button>
-                <button type='submit'>Cancel</button>
-            </div>
-        </form>
+        </Fragment>
     )
 }
 

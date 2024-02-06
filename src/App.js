664402ddcd815dcbe3f4e20544db80e48e73cb31
MadliReiljan <madlireiljan@gmail.com>
2024-02-06 13:16:15 +0200

    Ül 18

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
@@ -25,13 +25,20 @@ const DYMMY_EXPENSES=[
 ]
 
 const App = () => {
-  const [expenses, setExpenses] = useState(DYMMY_EXPENSES)
-
-const addExpenseHandler = (expense) => {
-  setExpenses((previousExpenses) => {
-    return[expense, ...expenses]
+  const [expenses, setExpenses] = useState(() => {
+    const expensesFromLS = JSON.parse(localStorage.getItem('expenses'));
+    return expensesFromLS || [];
   })
-}
+
+  useEffect (() => {
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  }, [expenses]);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((previousExpenses) => {
+      return[expense, ...expenses]
+    })
+  }
 
   return (
     <div className='App'>
